7f0bfef436b718f7d8eff271a9d49d33fe4cecf9
owaisazmal <owaiskhan461@gmail.com>
2024-07-26 11:41:45 -0700

    Loading view added

---
+++ WeatherApp/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by Khan, Owais on 7/26/24.
+//
+
+import Foundation
---
+++ WeatherApp/View/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Khan, Owais on 7/26/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
