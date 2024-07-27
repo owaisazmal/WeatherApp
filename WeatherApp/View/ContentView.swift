3e5353e960c36498c212aa8fc882cab97aa834ff
owaisazmal <owaiskhan461@gmail.com>
2024-07-27 12:48:54 -0700

    Weather Manager updated with API Key

--- WeatherApp/View/ContentView.swift
+++ WeatherApp/View/ContentView.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let location = locationManager.location{
+                Text("Your Coordinates are: \(location.longitude), \(location.latitude)")
+            }else{
+                if locationManager.isLoading{
+                    LoadingView()
+                }
+            }
+            WelcomeView()
+                .environmentObject(locationManager)
         }
-        .padding()
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
