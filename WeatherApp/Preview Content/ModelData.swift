893b4582511c0d13fdad3012e2a3541bcdc81698
owaisazmal <owaiskhan461@gmail.com>
2024-07-28 20:55:25 -0700

    API Integrated

---
+++ WeatherApp/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  WeatherApp
+//
+//  Created by Khan, Owais on 7/28/24.
+//
+
+import Foundation
---
+++ WeatherApp/View/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Khan, Owais on 7/27/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
